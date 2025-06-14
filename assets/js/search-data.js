6d1089f9e340fff91c88322833ff39743d82f91d
HegalBanjade <108894687+HegalBanjade@users.noreply.github.com>
2025-06-14 19:57:26 +0000

    Deploying to gh-pages from @ HegalBanjade/hegalbanjade.github.io@a8fc414e94473fac0ba7317acfa44be10243e14c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -35,30 +35,4 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{
-      id: 'light-theme',
-      title: 'Change theme to light',
-      description: 'Change the theme of the site to Light',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("light");
-      },
-    },
-    {
-      id: 'dark-theme',
-      title: 'Change theme to dark',
-      description: 'Change the theme of the site to Dark',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("dark");
-      },
-    },
-    {
-      id: 'system-theme',
-      title: 'Use system default theme',
-      description: 'Change the theme of the site to System Default',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("system");
-      },
-    },];
+            },},];
